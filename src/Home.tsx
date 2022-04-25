1983b9fe7d797d3b75ab3d80ed42c5e5b680f7a3
hoang nam <davidvunam@gmail.com>
2022-04-25 14:46:40 +0700

    test

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <>
-      <h1>Home page</h1>
+      <h1>Home page 1</h1>
       <p>
         Once you’ve created a new alias, it will be set as the current
         environment for deployment. Running firebase deploy will deploy your app
